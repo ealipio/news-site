ffa6b07bcc0cc476aaea5e616b7567f98881d262
Eisson <eisson.alipio@gmail.com>
2023-07-23 22:55:53 -0500

    fix: remove unused fetch access key

--- src/lib/fetchNews.ts
+++ src/lib/fetchNews.ts
@@ -7,16 +7,17 @@ export async function fetchNews(
   keywords?: string,
   isDynamic?: boolean,
 ) {
-  const accessKey =
-    process.env.MEDIA_STACK_KEY || "--to-deploy-without-errors--";
-  if (!accessKey) {
-    throw new Error("please check MEDIA_STACK_KEY environment variable");
-  }
+  // const accessKey =
+  //   process.env.MEDIA_STACK_KEY || "--to-deploy-without-errors--";
+  // if (!accessKey) {
+  //   throw new Error("please check MEDIA_STACK_KEY environment variable");
+  // }
   //const url = `http://api.mediastack.com/v1/news?access_key=${accessKey}`;
-  const response = await fetch(`${URL}/api?access_key=${accessKey}`, {
+  //const response = await fetch(`${URL}/api?access_key=${accessKey}`, {
+  const response = await fetch(`${URL}/api`, {
     method: "POST",
     cache: isDynamic ? "no-cache" : "default",
-    next: isDynamic ? { revalidate: 0 } : { revalidate: 30 },
+    next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
     headers: {
       "Content-Type": "application/json",
     },
